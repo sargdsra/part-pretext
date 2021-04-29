586cd0d4bf67f69bdc343ee503d6234896306c1c
sargdsra <sargdsra@gmail.com>
2021-04-30 00:54:54 +0430

    save state dict added.

--- part.py
+++ part.py
@@ -5,6 +5,7 @@ import torch.nn as nn
 from dataloader import PartLoader
 from model import Network
 from train import train_model
+from utils import save_state_dict
 
 
 data_file = 'images.txt'
@@ -13,7 +14,8 @@ lr = 0.001
 batch_size = 64
 epochs = 200
 model_path = None
-filename = 'checkpoint_part_resnet50.pth.tar'
+filename = 'checkpoint_part4_resnet50.pth.tar'
+state_dict_file_name = 'sd_part4_resnet50.pth'
 log_filename = 'log_part_resnet50.log'
 use_gpu = torch.cuda.is_available()
 num_workers = 3
@@ -37,3 +39,4 @@ else:
 net = net.to(device)
 criterion = nn.CrossEntropyLoss()
 train_model(net, criterion, optimizer, start_epoch, epochs, dataset, train_loader, device, filename, log_filename)
+save_state_dict(filename, state_dict_file_name)
--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import torch
+import copy
 
 
 def save_checkpoint(epoch, model, optimizer, filename):
@@ -6,3 +7,30 @@ def save_checkpoint(epoch, model, optimizer, filename):
              'model': model,
              'optimizer': optimizer}
     torch.save(state, filename)
+
+def save_state_dict(model_path, sd_filename):
+    checkpoint = torch.load(model_path)
+    net = checkpoint['model']
+    sd = net.state_dict()
+    new_sd = copy.deepcopy(sd)
+    for key in sd.keys():
+        new_key = key.replace('network.', '')
+        if new_key[0] == '0':
+            new_key = 'conv1' + new_key[1:]
+        if new_key[0] == '1':
+            new_key = 'bn1' + new_key[1:]
+        if new_key[0] == '2':
+            new_key = 'relu' + new_key[1:]
+        if new_key[0] == '3':
+            new_key = 'maxpool' + new_key[1:]
+        if new_key[0] == '4':
+            new_key = 'layer1' + new_key[1:]
+        if new_key[0] == '5':
+            new_key = 'layer2' + new_key[1:]
+        if new_key[0] == '6':
+            new_key = 'layer3' + new_key[1:]
+        if new_key[0] == '7':
+            new_key = 'layer4' + new_key[1:]
+        new_sd[new_key] = new_sd.pop(key)
+    torch.save(new_sd, filename)
+
